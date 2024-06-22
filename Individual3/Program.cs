77dad57cba5efa4de94d365daececb879076d7f0
mmf.lavrinen <mmf.lavrinen@MMF405-11>
2024-06-22 14:15:28 +0300

    Add project files.

---
+++ Individual3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите два двоичных числа разделенных '*'");
+string input  = Console.ReadLine();
+
+string[] numbers = input.Split('*');
+
+if  (numbers.Length != 2)
+{
+    Console.WriteLine("Некорректный ввод");
+    
+}
+
+string binary1 = numbers[0];
+string binary2 = numbers[1];
+
+int num1 = Convert.ToInt32(binary1);
+int num2 = Convert.ToInt32(binary2);
+
+int result = num1 - num2;
+
+if (result < 0)
+{
+    Console.WriteLine("Ошибка. Вычитание отрицательное");
+}
+else
+{
+    string binaryDifference = Convert.ToString(result, 2);
+
+    Console.WriteLine("Разность: " + binaryDifference);
+}
